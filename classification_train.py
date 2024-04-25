625632a6920b19679a2332c4d96b748cccb92533
CVliner <115476328+CVliner@users.noreply.github.com>
2024-04-26 02:11:02 +0500

    Add files via upload

---
+++ classification_train.py
@@ -0,0 +1,109 @@
+from tensorflow.keras.preprocessing.image import load_img
+
+base_dir = "C:/Downloads/Smile"
+happy_dir = os.path.join(base_dir, "happy/")
+sad_dir = os.path.join(base_dir, "sad/")
+
+print("Sample happy image:")
+plt.imshow(load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}"))
+plt.show()
+
+print("\nSample sad image:")
+plt.imshow(load_img(f"{os.path.join(sad_dir, os.listdir(sad_dir)[0])}"))
+plt.show()
+
+
+from tensorflow.keras.preprocessing.image import img_to_array
+
+# Load the first example of a happy face
+sample_image  = load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}")
+
+# Convert the image into its numpy array representation
+sample_array = img_to_array(sample_image)
+
+print(f"Each image has shape: {sample_array.shape}")
+
+print(f"The maximum pixel value used is: {np.max(sample_array)}")
+
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy') is not None and logs.get('accuracy') > 0.999:
+            print("\nReached 99.9% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def image_generator():
+    train_datagen = ImageDataGenerator(rescale=1./255.)
+
+    # Specify the method to load images from a directory and pass in the appropriate arguments:
+    # - directory: should be a relative path to the directory containing the data
+    # - targe_size: set this equal to the resolution of each image (excluding the color dimension)
+    # - batch_size: number of images the generator yields when asked for a next batch. Set this to 10.
+    # - class_mode: How the labels are represented. Should be one of "binary", "categorical" or "sparse".
+    #               Pick the one that better suits here given that the labels are going to be 1D binary labels.
+    train_generator = train_datagen.flow_from_directory(directory='C:/Downloads/Smile',
+                                                        target_size=(150, 150),
+                                                        batch_size=32,
+                                                        class_mode='binary')
+    ### END CODE HERE
+
+    return train_generator
+
+# Save your generator in a variable
+gen = image_generator()
+
+
+
+from tensorflow.keras import optimizers, losses
+
+def train_happy_sad_model(train_generator):
+
+    callbacks = myCallback()
+
+    model = tf.keras.models.Sequential([tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Dropout(0.6),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+
+    ])
+
+    # Compile the model
+    # Select a loss function compatible with the last layer of your network
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy']) 
+    
+
+
+    # Train the model
+    # Your model should achieve the desired accuracy in less than 15 epochs.
+    # You can hardcode up to 20 epochs in the function below but the callback should trigger before 15.
+    history = model.fit(x=train_generator,
+                        epochs=20,
+                        callbacks=[callbacks]
+                       ) 
+
+    return history
+
+hist = train_happy_sad_model(gen)
+
+"""If you see the message that was defined in the callback printed out after less than 15 epochs it means your callback worked as expected and training was successful. You can also double check by running the following cell:"""
+
+print(f"Your model reached the desired accuracy after {len(hist.epoch)} epochs")
+
+"""If your callback didn't stop training, one cause might be that you compiled your model using a metric other than `accuracy` (such as `acc`). Make sure you set the metric to `accuracy`. You can check by running the following cell:"""
+
+if not "accuracy" in hist.model.metrics_names:
+    print("Use 'accuracy' as metric when compiling your model.")
+else:
+    print("The metric was correctly defined.")
